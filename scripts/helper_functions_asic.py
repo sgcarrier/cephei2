8d836aa6f9a99d960a7b7c9281708795d3ee8e8c
Simon Carrier <simon.g.carrier@usherbrooke.ca>
2021-02-22 17:49:01 -0500

    Added np.int type for some arrays and fixed function calls for the PLL test

--- scripts/helper_functions_asic.py
+++ scripts/helper_functions_asic.py
@@ -211,7 +211,7 @@ class ASIC:
                 self.b.ICYSHSR1.PIXEL_DISABLE_TDC_ARRAY_1(self.head_id, disable_words[i], i)
 
     def set_quench_disable(self, array, disable_map):
-        disable_words = np.zeros(7)
+        disable_words = np.zeros(7, dtype=np.int)
         for i in range(len(disable_map)):
             word_index = int(i / 32)
             if disable_map[i]:
@@ -224,7 +224,7 @@ class ASIC:
                 self.b.ICYSHSR1.PIXEL_DISABLE_QUENCH_ARRAY_1(self.head_id, disable_words[i], i)
 
     def set_ext_trigger_disable(self, array, disable_map):
-        disable_words = np.zeros(7)
+        disable_words = np.zeros(7, dtype=np.int)
         for i in range(len(disable_map)):
             word_index = int(i / 32)
             if disable_map[i]:
@@ -245,7 +245,7 @@ class ASIC:
         array_size = 16
         if array == 0:
             array_size = 49
-        disable_map = np.zeros(array_size)
+        disable_map = np.zeros(array_size, dtype=np.int)
         for i in enabled_tdc:
             disable_map[i] = 1
         self.set_tdc_disable(array, disable_map)
@@ -255,7 +255,7 @@ class ASIC:
         array_size = 16
         if array == 0:
             array_size = 49
-        disable_map = np.ones(array_size)
+        disable_map = np.ones(array_size, dtype=np.int)
         for i in disabled_tdc:
             disable_map[i] = 0
         self.set_tdc_disable(array, disable_map)
@@ -264,7 +264,7 @@ class ASIC:
         array_size = 64
         if array == 0:
             array_size = 196
-        disable_map = np.zeros(array_size)
+        disable_map = np.zeros(array_size, dtype=np.int)
         for i in enabled_quench:
             disable_map[i] = 1
         self.set_quench_disable(array, disable_map)
@@ -273,7 +273,7 @@ class ASIC:
         array_size = 64
         if array == 0:
             array_size = 196
-        disable_map = np.ones(array_size)
+        disable_map = np.ones(array_size, dtype=np.int)
         for i in disabled_quench:
             disable_map[i] = 1
         self.set_quench_disable(array, disable_map)
@@ -282,7 +282,7 @@ class ASIC:
         array_size = 64
         if array == 0:
             array_size = 196
-        disable_map = np.zeros(array_size)
+        disable_map = np.zeros(array_size, dtype=np.int)
         for i in enabled_ext_trig:
             disable_map[i] = 1
         self.set_ext_trigger_disable(array, disable_map)
@@ -291,34 +291,34 @@ class ASIC:
         array_size = 64
         if array == 0:
             array_size = 196
-        disable_map = np.ones(array_size)
+        disable_map = np.ones(array_size, dtype=np.int)
         for i in disabled_ext_trig:
             disable_map[i] = 1
         self.set_ext_trigger_disable(array, disable_map)
 
     def enable_all_tdc(self):
-        self.b.ICYSHSR1.enable_all_tdc_but(0, [])
-        self.b.ICYSHSR1.enable_all_tdc_but(1, [])
+        self.enable_all_tdc_but(0, [])
+        self.enable_all_tdc_but(1, [])
 
     def enable_all_quench(self):
-        self.b.ICYSHSR1.enable_all_quench_but(0, [])
-        self.b.ICYSHSR1.enable_all_quench_but(1, [])
+        self.enable_all_quench_but(0, [])
+        self.enable_all_quench_but(1, [])
 
     def enable_all_ext_trigger(self):
-        self.b.ICYSHSR1.enable_all_ext_trigger_but(0, [])
-        self.b.ICYSHSR1.enable_all_ext_trigger_but(1, [])
+        self.enable_all_ext_trigger_but(0, [])
+        self.enable_all_ext_trigger_but(1, [])
 
     def disable_all_tdc(self):
-        self.b.ICYSHSR1.disable_all_tdc_but(0, [])
-        self.b.ICYSHSR1.disable_all_tdc_but(1, [])
+        self.disable_all_tdc_but(0, [])
+        self.disable_all_tdc_but(1, [])
 
     def disable_all_quench(self):
-        self.b.ICYSHSR1.disable_all_quench_but(0, [])
-        self.b.ICYSHSR1.disable_all_quench_but(1, [])
+        self.disable_all_quench_but(0, [])
+        self.disable_all_quench_but(1, [])
 
     def disable_all_ext_trigger(self):
-        self.b.ICYSHSR1.disable_all_ext_trigger_but(0, [])
-        self.b.ICYSHSR1.disable_all_ext_trigger_but(1, [])
+        self.disable_all_ext_trigger_but(0, [])
+        self.disable_all_ext_trigger_but(1, [])
 
     #
     # Configurations
@@ -336,31 +336,31 @@ class ASIC:
 
     #Setup pour test de la PLL lente; Input: Ref_Freq_Slow; Output: Div_Freq_Slow_PLL_Test
     def test_PLL_slow(self):
-        self.b.ICYSHSR1.PLL_ENABLE(self.head_id, 0) # Disable Array PLL
-        self.b.ICYSHSR1.PLL_ENABLE_TEST(self.head_id, 1) # Enable Test PLL
-        self.b.ICYSHSR1.PLL_SELECT_TEST(self.head_id, 1) # Use PLL for test
-        self.b.ICYSHSR1.PLL_TEST_SECTION_MUX(self.head_id, 0x0001) # Output freq_slow
+        self.b.ICYSHSR1.PLL_ENABLE(self.head_id, 0, 0) # Disable Array PLL
+        self.b.ICYSHSR1.PLL_ENABLE_TEST(self.head_id, 1, 0) # Enable Test PLL
+        self.b.ICYSHSR1.PLL_SELECT_TEST(self.head_id, 1, 0) # Use PLL for test
+        self.b.ICYSHSR1.PLL_TEST_SECTION_MUX(self.head_id, 0x0001, 0) # Output freq_slow
 
     # Setup pour test de la PLL Rapide; Input: Ref_Freq_Fast; Output: Div_Freq_Fast_PLL_Test
     def test_PLL_fast(self):
-        self.b.ICYSHSR1.PLL_ENABLE(self.head_id, 0) # Disable Array PLL
-        self.b.ICYSHSR1.PLL_ENABLE_TEST(self.head_id, 1) # Enable Test PLL
-        self.b.ICYSHSR1.PLL_SELECT_TEST(self.head_id, 1) # Use PLL for test
-        self.b.ICYSHSR1.PLL_TEST_SECTION_MUX(self.head_id, 0x0002) # Output freq_fast
+        self.b.ICYSHSR1.PLL_ENABLE(self.head_id, 0, 0) # Disable Array PLL
+        self.b.ICYSHSR1.PLL_ENABLE_TEST(self.head_id, 1, 0) # Enable Test PLL
+        self.b.ICYSHSR1.PLL_SELECT_TEST(self.head_id, 1, 0) # Use PLL for test
+        self.b.ICYSHSR1.PLL_TEST_SECTION_MUX(self.head_id, 0x0002, 0) # Output freq_fast
 
     # Setup pour test du TDC Test avec PLL de Test; Input: Ref_Freq_Slow, Ref_Freq_Fast; Output: o_serialiseur_test
     def test_TDC_PLL(self):
-        self.b.ICYSHSR1.PLL_ENABLE(self.head_id, 0) # Disable Array PLL
-        self.b.ICYSHSR1.PLL_ENABLE_TEST(self.head_id, 1) # Enable Test PLL
-        self.b.ICYSHSR1.PLL_SELECT_TEST(self.head_id, 1) # Use PLL for test
-        self.b.ICYSHSR1.PLL_TEST_SECTION_MUX(self.head_id, 0x0003) # Output TDC output
+        self.b.ICYSHSR1.PLL_ENABLE(self.head_id, 0, 0) # Disable Array PLL
+        self.b.ICYSHSR1.PLL_ENABLE_TEST(self.head_id, 1, 0) # Enable Test PLL
+        self.b.ICYSHSR1.PLL_SELECT_TEST(self.head_id, 1, 0) # Use PLL for test
+        self.b.ICYSHSR1.PLL_TEST_SECTION_MUX(self.head_id, 0x0003, 0) # Output TDC output
 
     # Setup pour test du TDC Test avec tension externe; Input: V_Fast, V_Slow; Output: o_serialiseur_test
     def test_TDC_ext(self):
-        self.b.ICYSHSR1.PLL_ENABLE(self.head_id, 0) # Disable Array PLL
-        self.b.ICYSHSR1.PLL_ENABLE_TEST(self.head_id, 1) # Enable Test PLL
-        self.b.ICYSHSR1.PLL_SELECT_TEST(self.head_id, 0) # Use Vext for test
-        self.b.ICYSHSR1.PLL_TEST_SECTION_MUX(self.head_id, 0x0003) # Output TDC output
+        self.b.ICYSHSR1.PLL_ENABLE(self.head_id, 0, 0) # Disable Array PLL
+        self.b.ICYSHSR1.PLL_ENABLE_TEST(self.head_id, 1, 0) # Enable Test PLL
+        self.b.ICYSHSR1.PLL_SELECT_TEST(self.head_id, 0, 0) # Use Vext for test
+        self.b.ICYSHSR1.PLL_TEST_SECTION_MUX(self.head_id, 0x0003, 0) # Output TDC output
 
     # Readout_time is in clk cycle (4ns)
     def configure_ct_counting_mode(self, array, readout_time):
